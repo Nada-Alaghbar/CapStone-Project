96bbad040bc019f43c9fb92b5323c89aff6921fc
Nada-Alaghbar <nada.alaghbar@gmail.com>
2023-01-27 14:05:38 +0300

    add-new component

--- src/app/Pages/admin/admin.module.ts
+++ src/app/Pages/admin/admin.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { CrudComponent } from './crud/crud.component';
+import { AddNewComponent } from './add-new/add-new.component';
 
 
 
 @NgModule({
   declarations: [
     CrudComponent,
+    AddNewComponent,
 
   ],
   imports: [
